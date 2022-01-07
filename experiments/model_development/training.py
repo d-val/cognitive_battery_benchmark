a3bd507ff9165cf0ae843060f4e1444c5fa196bf
neil-argus <neil.nitin.de@Gmail.com>
2022-01-06 23:02:38 -0500

    Updated model training script

--- experiments/model_development/training.py
+++ experiments/model_development/training.py
@@ -10,6 +10,8 @@ from torch import nn
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 
+from experiments.model_development.model import ImageClassifier
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using {} device".format(device))
 
@@ -27,58 +29,12 @@ for child in resnet18.children():
 
 model = resnet18
 
-loss_fn = nn.CrossEntropyLoss()
+classifier = ImageClassifier(model, loss_fn=nn.CrossEntropyLoss(), num_classes=3, freeze_layers=9)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
+train_dataloader, test_dataloader = None, None # TODO: load data
 
-def train_loop(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
-        # Compute prediction and loss
-        pred = model(X)
-        loss = loss_fn(pred, torch.flatten(y))
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-    return loss
-
-
-def test_loop(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    test_loss, correct = 0, 0
-
-    with torch.no_grad():
-        for X, y in dataloader:
-            pred = model(X)
-            test_loss += loss_fn(pred, torch.flatten(y)).item()
-            correct += (
-                (pred.argmax(1) == torch.flatten(y)).type(torch.float).sum().item()
-            )
-
-    test_loss /= num_batches
-    print(size, correct)
-    correct /= size
-    print(
-        f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
-    )
-    return test_loss, correct
-
-
-epochs = 10
-test_l = acc = []
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train_loop(train_dataloader, model, loss_fn, optimizer)
-    test_loss, correct = test_loop(test_dataloader, model, loss_fn)
-    test_l.append(test_loss)
-    acc.append(correct)
+classifier.train(train_dataloader=train_dataloader, test_dataloader=test_dataloader, save_path="experiments/model_development/output_models/resnet18_9_layers_frozen.pt")
 print("Done!")
 
 torch.save(model.state_dict(), "output")
