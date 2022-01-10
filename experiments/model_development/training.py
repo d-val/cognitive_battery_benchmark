34e34000fdce26db0360c6b6b77d3edbbc1c0b1c
neil-argus <neil.nitin.de@Gmail.com>
2022-01-09 20:36:45 -0500

    Reformatted code

--- experiments/model_development/training.py
+++ experiments/model_development/training.py
@@ -29,12 +29,18 @@ for child in resnet18.children():
 
 model = resnet18
 
-classifier = ImageClassifier(model, loss_fn=nn.CrossEntropyLoss(), num_classes=3, freeze_layers=9)
+classifier = ImageClassifier(
+    model, loss_fn=nn.CrossEntropyLoss(), num_classes=3, freeze_layers=9
+)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-train_dataloader, test_dataloader = None, None # TODO: load data
+train_dataloader, test_dataloader = None, None  # TODO: load data
 
-classifier.train(train_dataloader=train_dataloader, test_dataloader=test_dataloader, save_path="experiments/model_development/output_models/resnet18_9_layers_frozen.pt")
+classifier.train(
+    train_dataloader=train_dataloader,
+    test_dataloader=test_dataloader,
+    save_path="experiments/model_development/output_models/resnet18_9_layers_frozen.pt",
+)
 print("Done!")
 
 torch.save(model.state_dict(), "output")
