f39c49810e00555bd018c891c1c78e67015ce230
neil-argus <neil.nitin.de@Gmail.com>
2022-01-11 15:59:10 -0500

    Added data loader class for model pipeline

--- experiments/model_development/model.py
+++ experiments/model_development/model.py
@@ -98,3 +98,25 @@ class ImageClassifier:
             f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
         )
         return test_loss, correct
+
+class BenchmarkDataset(Dataset):
+  def __init__(self, folder, experiment, transform=None, target_transform=None):
+    super(BenchmarkDataset).__init__()
+    self.folder = folder
+    self.experiment = experiment
+    self.transform = transform
+    self.target_transform = target_transform
+    self.len = len(next(os.walk(os.path.join(self.folder, experiment)))[1])
+
+  def __len__(self):
+    return self.len
+
+  def __getitem__(self, idx):
+    with open(f"{self.folder}/{self.experiment}/{idx}/machine_readable/iteration_data.pickle", "rb") as iter_file:
+      iter_data = pickle.load(iter_file)
+    images, label = torch.from_numpy(np.moveaxis(iter_data['images'][0], 2, 0)), iter_data['label']
+    if self.transform:
+        images = self.transform(images)
+    if self.target_transform:
+        label = self.target_transform(label)
+    return images, label
