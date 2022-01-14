82e544abc77ff066e879b6baac983d8fbbeb5570
dhaval <nuclearr.d@gmail.com>
2022-01-14 15:07:03 -0500

    updating readme

--- experiments/dataset_generation/simple_swap_example.py
+++ experiments/dataset_generation/simple_swap_example.py
@@ -2,4 +2,5 @@ from simple_swap import SimpleSwap
 
 SimpleSwapExperiment = SimpleSwap()
 SimpleSwapExperiment.run()
+### UNCOMMENT THE FOLLOWING LINE TO SAVE IMAGES
 SimpleSwapExperiment.save_frames_to_folder('output')
