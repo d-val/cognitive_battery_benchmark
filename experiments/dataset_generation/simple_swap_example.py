a8763bc29f5c9fcb1c26e339eafabbaa572abdb0
Teresa Gao <t_gao@mit.edu>
2022-05-31 23:37:20 -0700

    clarify instructions for simulation setup

--- experiments/dataset_generation/simple_swap_example.py
+++ experiments/dataset_generation/simple_swap_example.py
@@ -2,5 +2,5 @@ from utils.experiment_tasks.simple_swap import SimpleSwap
 
 SimpleSwapExperiment = SimpleSwap(seed=100)
 SimpleSwapExperiment.run()
-### UNCOMMENT THE FOLLOWING LINE TO SAVE IMAGES
+### THE FOLLOWING LINE SAVES IMAGES OF THE SIMULATION
 SimpleSwapExperiment.save_frames_to_folder("output/simple_swap_example")
