eaa40dda4ca47873ddaef981cede4ef80e160612
Teresa Gao <t_gao@mit.edu>
2022-07-25 00:25:41 -0700

    fix mmcv_custom import

--- experiments/model_development/utils/models/Video_Swin_Transformer/mmaction/apis/train.py
+++ experiments/model_development/utils/models/Video_Swin_Transformer/mmaction/apis/train.py
@@ -12,7 +12,7 @@ from ..core import (DistEvalHook, EvalHook, OmniSourceDistSamplerSeedHook,
 from ..datasets import build_dataloader, build_dataset
 from ..utils import PreciseBNHook, get_root_logger
 from .test import multi_gpu_test
-from mmcv_custom.runner import EpochBasedRunnerAmp
+from utils.models.Video_Swin_Transformer.mmcv_custom.runner import EpochBasedRunnerAmp
 import apex
 import os.path as osp
 
