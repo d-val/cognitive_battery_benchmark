4f48fd789794d23cefd3a94aec5c11824365a75b
Teresa Gao <t_gao@mit.edu>
2022-08-18 05:52:43 -0700

    update training commands in .sh file

--- experiments/model_development/utils/models/Video_Swin_Transformer/train.sh
+++ experiments/model_development/utils/models/Video_Swin_Transformer/train.sh
@@ -3,8 +3,10 @@ python3 prepare_data.py
 echo "completed preparation of data"
 
 # TODO: uncomment!
-# echo "beginning training"
-# python3 tools/train.py configs/recognition/swin/swin_base_patch244_window877_kinetics400_22k.py --resume-from pretrained/swin_base_patch244_window877_kinetics400_22k.pth --validate --test-best --seed 22 --deterministic
-# echo "completed training"
+echo "beginning training"
+timestamp=$(date "+%Y-%m-%d_%H-%M-%S")
+results_filename=$timestamp.pkl
+python3 tools/train.py configs/recognition/swin/swin_base_patch244_window877_kinetics400_22k.py --resume-from pretrained/swin_base_patch244_window877_kinetics400_22k.pth --validate --test-best --seed 22 --deterministic --out $results_filename
+echo "completed training"
 
 echo "\nall done :)"
