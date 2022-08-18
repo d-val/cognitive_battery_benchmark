80788d69c4210d7afcc749c6ba71c9ae9791aca6
Teresa Gao <t_gao@mit.edu>
2022-08-18 06:17:30 -0700

    remove legacy flag from command line arguments

--- experiments/model_development/utils/models/Video_Swin_Transformer/train.sh
+++ experiments/model_development/utils/models/Video_Swin_Transformer/train.sh
@@ -4,9 +4,7 @@ echo "completed preparation of data"
 
 # TODO: uncomment!
 echo "beginning training"
-timestamp=$(date "+%Y-%m-%d_%H-%M-%S")
-results_filename=$timestamp.pkl
-python3 tools/train.py configs/recognition/swin/swin_base_patch244_window877_kinetics400_22k.py --resume-from pretrained/swin_base_patch244_window877_kinetics400_22k.pth --validate --test-best --seed 22 --deterministic --out $results_filename
+python3 tools/train.py configs/recognition/swin/swin_base_patch244_window877_kinetics400_22k.py --resume-from pretrained/swin_base_patch244_window877_kinetics400_22k.pth --validate --test-best --seed 22 --deterministic
 echo "completed training"
 
 echo "\nall done :)"
