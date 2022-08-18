4de368ca73e96a3e61ba80300a29f25666396063
Teresa Gao <t_gao@mit.edu>
2022-08-18 05:41:55 -0700

    add script for dataset generation and annotation file setup

---
+++ experiments/model_development/utils/models/Video_Swin_Transformer/train.sh
@@ -0,0 +1,10 @@
+echo "beginning preparation of data"
+python3 prepare_data.py
+echo "completed preparation of data"
+
+# TODO: uncomment!
+# echo "beginning training"
+# python3 tools/train.py configs/recognition/swin/swin_base_patch244_window877_kinetics400_22k.py --resume-from pretrained/swin_base_patch244_window877_kinetics400_22k.pth --validate --test-best --seed 22 --deterministic
+# echo "completed training"
+
+echo "\nall done :)"
