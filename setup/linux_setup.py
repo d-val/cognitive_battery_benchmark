89c7a503afc6afb3579930ed21f794a236979691
neil-argus <neil.nitin.de@Gmail.com>
2022-11-07 02:09:07 -0500

    updated packages

--- setup/linux_setup.py
+++ setup/linux_setup.py
@@ -1,15 +1,9 @@
 from IPython.display import HTML, display
 import sys
 
-from moviepy.editor import ImageSequenceClip
-from typing import Sequence
 import numpy as np
 import os
-from typing import Optional
 import ai2thor.server
-from typing import Union
-from PIL import Image
-
 def start_xserver() -> None:
     """Provide the ability to render AI2-THOR using Google Colab."""
 
@@ -27,71 +21,64 @@ def start_xserver() -> None:
 
     progress_bar = display(progress(0), display_id=True)
 
-    try:
-        import google.colab
 
-        using_colab = True
-    except ImportError:
-        using_colab = False
-
-    if using_colab:
-        with open("frame-buffer", "w") as writefile:
-            writefile.write(
-                """#taken from https://gist.github.com/jterrace/2911875
-        XVFB=/usr/bin/Xvfb
-        XVFBARGS=":1 -screen 0 1024x768x24 -ac +extension GLX +render -noreset"
-        PIDFILE=./frame-buffer.pid
-        case "$1" in
-        start)
-            /sbin/start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
-            ;;
-        stop)
-            /sbin/start-stop-daemon --stop --quiet --pidfile $PIDFILE
-            rm $PIDFILE
-            ;;
-        restart)
-            $0 stop
-            $0 start
-            ;;
-        *)
-                exit 1
-        esac
-        exit 0
-            """
-            )
-
-        progress_bar.update(progress(5))
-        os.system("apt-get install daemon >/dev/null 2>&1")
-
-        progress_bar.update(progress(10))
-        os.system("apt-get install wget >/dev/null 2>&1")
-
-        progress_bar.update(progress(20))
-        os.system(
-            "wget http://ai2thor.allenai.org/ai2thor-colab/libxfont1_1.5.1-1ubuntu0.16.04.4_amd64.deb >/dev/null 2>&1"
+    with open("frame-buffer", "w") as writefile:
+        writefile.write(
+            """#taken from https://gist.github.com/jterrace/2911875
+    XVFB=/usr/bin/Xvfb
+    XVFBARGS=":1 -screen 0 1024x768x24 -ac +extension GLX +render -noreset"
+    PIDFILE=./frame-buffer.pid
+    case "$1" in
+    start)
+        /sbin/start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
+        ;;
+    stop)
+        /sbin/start-stop-daemon --stop --quiet --pidfile $PIDFILE
+        rm $PIDFILE
+        ;;
+    restart)
+        $0 stop
+        $0 start
+        ;;
+    *)
+            exit 1
+    esac
+    exit 0
+        """
         )
 
-        progress_bar.update(progress(30))
-        os.system(
-            "wget --output-document xvfb.deb http://ai2thor.allenai.org/ai2thor-colab/xvfb_1.18.4-0ubuntu0.12_amd64.deb >/dev/null 2>&1"
-        )
+    progress_bar.update(progress(5))
+    os.system("apt-get install daemon >/dev/null 2>&1")
 
-        progress_bar.update(progress(40))
-        os.system("dpkg -i libxfont1_1.5.1-1ubuntu0.16.04.4_amd64.deb >/dev/null 2>&1")
+    progress_bar.update(progress(10))
+    os.system("apt-get install wget >/dev/null 2>&1")
 
-        progress_bar.update(progress(50))
-        os.system("dpkg -i xvfb.deb >/dev/null 2>&1")
+    progress_bar.update(progress(20))
+    os.system(
+        "wget http://ai2thor.allenai.org/ai2thor-colab/libxfont1_1.5.1-1ubuntu0.16.04.4_amd64.deb >/dev/null 2>&1"
+    )
 
-        progress_bar.update(progress(70))
-        os.system("rm libxfont1_1.5.1-1ubuntu0.16.04.4_amd64.deb")
+    progress_bar.update(progress(30))
+    os.system(
+        "wget --output-document xvfb.deb http://ai2thor.allenai.org/ai2thor-colab/xvfb_1.18.4-0ubuntu0.12_amd64.deb >/dev/null 2>&1"
+    )
 
-        progress_bar.update(progress(80))
-        os.system("rm xvfb.deb")
+    progress_bar.update(progress(40))
+    os.system("dpkg -i libxfont1_1.5.1-1ubuntu0.16.04.4_amd64.deb >/dev/null 2>&1")
 
-        progress_bar.update(progress(90))
-        os.system("bash frame-buffer start")
+    progress_bar.update(progress(50))
+    os.system("dpkg -i xvfb.deb >/dev/null 2>&1")
 
-        os.environ["DISPLAY"] = ":1"
+    progress_bar.update(progress(70))
+    os.system("rm libxfont1_1.5.1-1ubuntu0.16.04.4_amd64.deb")
+
+    progress_bar.update(progress(80))
+    os.system("rm xvfb.deb")
+
+    progress_bar.update(progress(90))
+    os.system("bash frame-buffer start")
+
+    os.environ["DISPLAY"] = ":1"
     progress_bar.update(progress(100))
 
 
