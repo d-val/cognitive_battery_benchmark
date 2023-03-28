78db1260fbd02920617d6470a38461d1c014480c
neil-argus <neild@mit.edu>
2023-03-28 19:39:41 -0400

    updating configs

--- experiments/dataset_generation/relative_numbers_dataset.py
+++ experiments/dataset_generation/relative_numbers_dataset.py
@@ -1,4 +1,4 @@
 from utils.experiment_job import ExperimentJob
 
-expt = ExperimentJob("config/renderer.yaml", ["config/RelativeNumbers_config.yaml"])
+expt = ExperimentJob("config/linux_renderer.yaml", ["config/RelativeNumbers_config.yaml"])
 expt.run()
--- experiments/dataset_generation/rotation_dataset.py
+++ experiments/dataset_generation/rotation_dataset.py
@@ -1,4 +1,4 @@
 from utils.experiment_job import ExperimentJob
 
-expt = ExperimentJob("config/renderer.yaml", ["config/RotationChain_config.yaml"])
+expt = ExperimentJob("config/linux_renderer.yaml", ["config/RotationChain_config.yaml"])
 expt.run()
