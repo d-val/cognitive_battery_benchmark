8b52f957c952816f3198fe6ed3f4266a059a0f2b
Teresa Gao <t_gao@mit.edu>
2023-04-06 12:44:20 -0400

    intermediate (not fully functional) commit to add progress on VideoMAE model

--- experiments/model_development/train.py
+++ experiments/model_development/train.py
@@ -5,7 +5,7 @@ train.py: script to run a training job using config/train.yaml
 from utils.train_job import TrainingConfig, TrainingJob
 
 # First, load the training config
-config = TrainingConfig.from_yaml("config/config.yaml")
+config = TrainingConfig.from_yaml("config/train.yaml")
 
 # Construct a training job and run it with per-epoch evaluation.
 job = TrainingJob(config=config)
