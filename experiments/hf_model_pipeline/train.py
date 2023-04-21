3b23b179ac72136073817346f53f48ef77704e83
neil-argus <neild@mit.edu>
2023-04-20 23:27:07 -0400

    reduced batch size

--- experiments/hf_model_pipeline/train.py
+++ experiments/hf_model_pipeline/train.py
@@ -15,5 +15,5 @@ model = VideoMAEForVideoClassification.from_pretrained(
 dataset.preprocess(preprocessor, model)
 train_pipeline = TrainModelPipeline(preprocessor, model, dataset)
 
-train_pipeline.train(3, 6)
-train_pipeline.test(6)
+train_pipeline.train(3, 4)
+train_pipeline.test(4)
