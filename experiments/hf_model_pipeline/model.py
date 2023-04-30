071da1ee395bd4fcf97932d00d2fe48208ef283e
neil-argus <neild@mit.edu>
2023-04-30 15:56:34 -0400

    updated model configs

--- experiments/hf_model_pipeline/model.py
+++ experiments/hf_model_pipeline/model.py
@@ -1,5 +1,5 @@
 from transformers import AutoFeatureExtractor, AutoModelForVideoClassification
-from transformers import VideoMAEImageProcessor, VideoMAEForVideoClassification
+from transformers import VideoMAEImageProcessor, VideoMAEForVideoClassification, AutoProcessor, AutoModel
 
 
 class VideoMAE():
@@ -29,8 +29,8 @@ class Timesformer():
 class XClip():
     def __init__(self, dataset):
         model_ckpt = "microsoft/xclip-base-patch32"
-        self.preprocessor = AutoFeatureExtractor.from_pretrained(model_ckpt)
-        self.model = AutoModelForVideoClassification.from_pretrained(
+        self.preprocessor = AutoProcessor.from_pretrained(model_ckpt)
+        self.model = AutoModel.from_pretrained(
             model_ckpt,
             label2id=dataset.label2id,
             id2label=dataset.id2label,
